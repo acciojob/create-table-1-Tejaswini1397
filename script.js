a2700d86346326e58c1b9bff7560f6d1bfb96139
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-29 14:28:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function insert_Row() {
- let table=document.getElementById("sampleTable")
-	let tr=document.createElement("tr");
-	let td1=document.createElement("td");
-	let td2=document.createElement("td");
+ // let table=document.getElementById("sampleTable")
+	// let tr=document.createElement("tr");
+	// let td1=document.createElement("td");
+	// let td2=document.createElement("td");
 
-  // Set the values for the new cells
-  td1.innerText = "New Cell1";
-  td2.innerText = "New Cell2";
-	tr.appendChild(td1);
-	tr.appendChild(td2);
-	table.prepend(tr);
+ //  // Set the values for the new cells
+ //  td1.innerText = "New Cell1";
+ //  td2.innerText = "New Cell2";
+	// tr.appendChild(td1);
+	// tr.appendChild(td2);
+	// table.prepend(tr);
 
-  
+    let table = document.getElementById("sampleTable")
+	let tr = document.createElement("tr")
+	let td1 = document.createElement("td")
+	let td2 = document.createElement("td")
+  td1.innerText = "New Cell1"
+	td2.innerText = "New Cell2"
+	tr.appendChild(td1)
+	tr.appendChild(td2)
+	table.prepend(tr)
 }
